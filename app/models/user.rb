327b45b00d2d459acf446a1f6252348c4b260068
Seamus McGill <seamusmcgill@gmail.com>
2022-03-14 19:40:32 -0400

    Add validations for user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :name, :email, :password, :password_confirmation, presence: true
+  # Convert all entered 
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true, length: { minimum: 6 }
+
   
 end
