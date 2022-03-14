2a0c54e88f51b6857a26e51b0c483379e624ee18
Seamus McGill <seamusmcgill@gmail.com>
2022-03-14 19:40:48 -0400

    Add custom class method to authenticate with credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,16 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, confirmation: true, length: { minimum: 6 }
 
-  
+  # Force email to downcase before saving
+  before_save { self.email = email.downcase }
+
+  def self.authenticate_with_credentials(email, password)
+    # Check if email stripped of white space and downcased matches password
+    if user = User.find_by_email(email.strip.downcase)&.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
